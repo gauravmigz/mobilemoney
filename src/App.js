dc36367a7c44952b70e457969a02b582ba5c8a49
gauravmigz <gauravmigz@gmail.com>
2021-08-21 15:19:51 +0530

    Added Deployment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Must Read React 
+          Learn React 
         </a>
       </header>
     </div>
