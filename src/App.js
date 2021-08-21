a75191303cc1fbf52261d202338d2094b808dfe3
gauravmigz <gauravmigz@gmail.com>
2021-08-21 14:18:06 +0530

    Only Build and Test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Must Read React 
         </a>
       </header>
     </div>
